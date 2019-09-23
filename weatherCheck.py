e695448c7c14ea6d0abc1396fb3c936569da75d1
bfrentz <bryce.frentz@gmail.com>
2019-09-23 15:45:20 -0400

    Created a weather checker

---
+++ weatherCheck.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# weatherCheck.py - Opens a wunderground site for Notre Dame weather
+#
+# Bryce Frentz
+# 9/19/19
+
+import webbrowser, sys, pyperclip
+
+webbrowser.open('https://www.wunderground.com/weather/us/in/notre-dame')

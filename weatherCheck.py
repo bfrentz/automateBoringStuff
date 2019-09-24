1a185132faadf0261c5b5a7b4eb55472f3af3b95
bfrentz <bryce.frentz@gmail.com>
2019-09-24 11:21:19 -0400

    Planning for additions to weatherCheck.py

--- weatherCheck.py
+++ weatherCheck.py
@@ -7,3 +7,6 @@
 import webbrowser, sys, pyperclip
 
 webbrowser.open('https://www.wunderground.com/weather/us/in/notre-dame')
+
+# I'd also like to update this to take a command line argument for different places about which I care (ND, Louisville, Sioux Falls, Bozeman, MSP)
+# I want to update using beautifulsoup4 to get hourly weather for the day or the 10 day forecast

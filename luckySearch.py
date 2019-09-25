91aee5a780f1df632642f562f46bb538fd9c2e09
bfrentz <bryce.frentz@gmail.com>
2019-09-25 11:34:34 -0400

    Debugging and trying to get first five links accurately

--- luckySearch.py
+++ luckySearch.py
@@ -7,16 +7,29 @@
 import requests, sys, webbrowser, bs4
 
 print('Googling...') 	# display text while downloading the google page
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
-res.raise_for_status()
+url = 'https://google.com/search?q=' + ' '.join(sys.argv[1:])
+print(f'url: {url}')
+browser = requests.get(url)
+browser.raise_for_status()
 
+# DEBUG
+print(browser.content)
 
 # Retrieve top search result links
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(browser.text, features="lxml")
+#print(soup.getText())
 
 
 # Open a browswer tab for each result
-linkElems = soup.select('.r a')
+linkElems = soup.select('div.r a')
+
+# DEBUG
+print(len(linkElems))
+for item in range(len(linkElems)):
+	print(str(item) + "\t" + linkElems[item].getText())
+
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
+	print(i)
+	print(linkElems[i].get('href'))
 	webbrowser.open('http://google.com' + linkElems[i].get('href'))

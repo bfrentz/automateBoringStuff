0292eb803fe88b876ec42f35ee01b4525c2be338
bfrentz <bryce.frentz@gmail.com>
2019-09-25 10:27:41 -0400

    Now retrieves results and opens them

--- luckySearch.py
+++ luckySearch.py
@@ -10,7 +10,13 @@ print('Googling...') 	# display text while downloading the google page
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
+
 # Retrieve top search result links
+soup = bs4.BeautifulSoup(res.text)
 
 
 # Open a browswer tab for each result
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+	webbrowser.open('http://google.com' + linkElems[i].get('href'))
